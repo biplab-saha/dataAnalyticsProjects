411cb14538dd6ba3c1a10b7ca836db8dcdb5931d
biplab5527 <allinformation5527x@gmail.com>
2026-03-04 13:36:11 +0530

    add few line code

--- firstProjects.py
+++ firstProjects.py
@@ -7,9 +7,14 @@ import seaborn as sns
 df = pd.read_csv('Diwali Sales Data.csv', encoding='unicode_escape')
 # to avoide encoding error , use 'unicode_escape'
 
-print(df.shape)
-print(df.head(50))
-print(df.tail(10))
+# print(df.shape)
+# print(df.head(50))
+# print(df.tail(10))
 
+
+removeUnusedCloumn = df.drop(['Status','unnamed1'],axis=1, inplace= True)
+print(removeUnusedCloumn)
+
+print(df.info())
 
 
